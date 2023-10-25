549ad72fa4db510cbf5a9f4a48ffc85bb4885753
Saad <blackavenger369@gmail.com>
2023-10-25 14:18:24 +0500

    bash added

---
+++ script.bash
@@ -0,0 +1,9 @@
+git filter-branch --commit-filter '
+if [ "$GIT_AUTHOR_NAME" = "blackspider1996" ];
+then
+    GIT_AUTHOR_NAME="blackavenger369";
+    GIT_AUTHOR_EMAIL="blackavenger369@gmail.com";
+    git commit-tree "$@";
+else
+    git commit-tree "$@";
+fi' HEAD
